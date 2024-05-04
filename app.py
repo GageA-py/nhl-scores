1df18e03958db2f3da43dbb59d98651ce36f6f19
GageA-py <gage_a6969@proton.me>
2024-05-04 10:54:39 -0400

    reformatted code into function

--- app.py
+++ app.py
@@ -5,74 +5,46 @@ app = Flask(__name__)
 response = requests.get('https://api-web.nhle.com/v1/scoreboard/now')
 scores = response.json()
 
+def generate_game_data():
+    latest_games_data = {}
+    for date_info in scores['gamesByDate']:
+        for game in date_info['games']:
+            if 'score' in game['homeTeam'] and 'score' in game['awayTeam']:
+                series_key = f"{game['homeTeam']['abbrev']} {game['awayTeam']['abbrev']}"
+                game_number = game['seriesStatus']['game']
+                home_team = f"{game['homeTeam']['abbrev']}"
+                home_team_logo = f"{game['homeTeam']['logo']}"
+                home_team_score = f"{game['homeTeam']['score']}"
+                away_team = f"{game['awayTeam']['abbrev']}"
+                away_team_logo = f"{game['awayTeam']['logo']}"
+                away_team_score = f"{game['awayTeam']['score']}"
+                series_game_number = f"Game {game_number}"
+            
+                if home_team_score > away_team_score:
+                    scoreboard = f"{home_team} DEF. {away_team} {home_team_score}-{away_team_score}"
+                elif away_team_score > home_team_score:
+                    scoreboard = f"{away_team} DEF. {home_team} {away_team_score}-{home_team_score}"
+                else:
+                    scoreboard = f"Game is tied."
+
+                latest_games_data[series_key] = {'home_team': home_team,
+                'away_team': away_team,
+                'series_game_number': series_game_number,
+                'home_team_logo_url': home_team_logo,
+                'home_team_score': home_team_score,
+                'away_team_score': away_team_score,
+                'away_team_logo_url': away_team_logo,
+                'scoreboard': scoreboard}
+    return latest_games_data
+
 
 @app.route("/")
 def index():
-    latest_games_data = {}
-    for date_info in scores['gamesByDate']:
-        for game in date_info['games']:
-            if 'score' in game['homeTeam'] and 'score' in game['awayTeam']:
-                series_key = f"{game['homeTeam']['abbrev']} {game['awayTeam']['abbrev']}"
-                game_number = game['seriesStatus']['game']
-                home_team = f"{game['homeTeam']['abbrev']}"
-                home_team_logo = f"{game['homeTeam']['logo']}"
-                home_team_score = f"{game['homeTeam']['score']}"
-                away_team = f"{game['awayTeam']['abbrev']}"
-                away_team_logo = f"{game['awayTeam']['logo']}"
-                away_team_score = f"{game['awayTeam']['score']}"
-                series_game_number = f"Game {game_number}"
-            
-                if home_team_score > away_team_score:
-                    scoreboard = f"{home_team} DEF. {away_team} {home_team_score}-{away_team_score}"
-                elif away_team_score > home_team_score:
-                    scoreboard = f"{away_team} DEF. {home_team} {away_team_score}-{home_team_score}"
-                else:
-                    scoreboard = f"Game is tied."
-
-                latest_games_data[series_key] = {'home_team': home_team,
-                'away_team': away_team,
-                'series_game_number': series_game_number,
-                'home_team_logo_url': home_team_logo,
-                'home_team_score': home_team_score,
-                'away_team_score': away_team_score,
-                'away_team_logo_url': away_team_logo,
-                'scoreboard': scoreboard}
-    
-    return render_template('scoreboard.html', latest_games_data=latest_games_data)
+    return render_template('scoreboard.html', latest_games_data=generate_game_data())
 
 @app.route('/now')
 def get_current_score():
-    latest_games_data = {}
-    for date_info in scores['gamesByDate']:
-        for game in date_info['games']:
-            if 'score' in game['homeTeam'] and 'score' in game['awayTeam']:
-                series_key = f"{game['homeTeam']['abbrev']} {game['awayTeam']['abbrev']}"
-                game_number = game['seriesStatus']['game']
-                home_team = f"{game['homeTeam']['abbrev']}"
-                home_team_logo = f"{game['homeTeam']['logo']}"
-                home_team_score = f"{game['homeTeam']['score']}"
-                away_team = f"{game['awayTeam']['abbrev']}"
-                away_team_logo = f"{game['awayTeam']['logo']}"
-                away_team_score = f"{game['awayTeam']['score']}"
-                series_game_number = f"Game {game_number}"
-            
-                if home_team_score > away_team_score:
-                    scoreboard = f"{home_team} DEF. {away_team} {home_team_score}-{away_team_score}"
-                elif away_team_score > home_team_score:
-                    scoreboard = f"{away_team} DEF. {home_team} {away_team_score}-{home_team_score}"
-                else:
-                    scoreboard = f"Game is tied."
-
-                latest_games_data[series_key] = {'home_team': home_team,
-                'away_team': away_team,
-                'series_game_number': series_game_number,
-                'home_team_logo_url': home_team_logo,
-                'home_team_score': home_team_score,
-                'away_team_score': away_team_score,
-                'away_team_logo_url': away_team_logo,
-                'scoreboard': scoreboard}
-                
-    return jsonify(latest_games_data)
+    return jsonify(generate_game_data())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
